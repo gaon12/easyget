e2cb3fbf7145daba65dd89d911ef9037781ceabb
easyget-cli <easyget@google.com>
2026-04-18 01:59:08 +0900

    chore: bump version to 1.0.3 and update publish workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="easyget",
-    version="1.0.1",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=[],
     entry_points={
